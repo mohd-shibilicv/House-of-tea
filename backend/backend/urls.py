ca8c98b53a1fb4cf85b72bb381d28dff86a4a11d
mohd-shibilicv <project.nasra@gmail.com>
2024-09-02 00:07:56 +0530

    prod: initial setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from .admin import admin_site
 from django.urls import include, path
 from django.conf.urls.static import static
@@ -21,9 +22,8 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
 urlpatterns = i18n_patterns(
-    path('admin/', admin_site.urls),
-    path('products/',include('products.urls')),
-    path('contact-us/',include('contact.urls')),
-    path('',include(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))),
-    )
-
+    path("admin/", admin_site.urls),
+    path("products/", include("products.urls")),
+    path("contact-us/", include("contact.urls")),
+    path("", include(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
